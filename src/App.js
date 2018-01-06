5df31fa551b71e23ccce356984ee99646ffb43f7
Jon <jonathan.howe.yu@gmail.com>
2018-01-06 01:45:56 -0500

    Basic calculator functionality, added tests

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ class App extends Component {
   constructor() {
     super();
     this.buttonClicked = this.buttonClicked.bind(this);
+    this.numberPressed = this.numberPressed.bind(this);
+    this.evalPrevious = this.evalPrevious.bind(this);
   }
 
   state = {
+    currentValue: 0,
     total: 0,
-    currentOperator: null
+    currentOperator: null,
+    showTotal: false
   }
 
   buttonClicked(buttonInput) {
@@ -18,44 +22,56 @@ class App extends Component {
         buttonInput === '-' ||
         buttonInput === '/' ||
         buttonInput === '*' ||
-        buttonInput === '=' ||
-        buttonInput === '!') {
-          this.setState({currentOperator: buttonInput});
+        buttonInput === '=') {
+      this.evalPrevious();
+      this.setState({currentValue: 0, currentOperator: buttonInput, showTotal: true});
     }
     else {
-      if (this.state.currentOperator === '+') {
-        this.setState(prevState => ({total: prevState.total + buttonInput}));
-      }
-      else if (this.state.currentOperator === '-') {
-        this.setState(prevState => ({total: prevState.total - buttonInput}));
-      }
-      else if (this.state.currentOperator === '/') {
-        this.setState(prevState => ({total: prevState.total / buttonInput}));
-      }
-      else if (this.state.currentOperator === '*') {
-        this.setState(prevState => ({total: prevState.total * buttonInput}));
-      }
-      else {
-        this.setState({total: buttonInput});
-      }
+      this.numberPressed(buttonInput);
+    }
+  }
+
+  numberPressed(number) {
+    this.setState(prevState => ({currentValue: prevState.currentValue * 10 + number, showTotal: false}));
+  }
+
+  evalPrevious() {
+    if (this.state.currentOperator === '+') {
+      this.setState(prevState => ({total: prevState.total + prevState.currentValue}));
+    }
+    else if (this.state.currentOperator === '-') {
+      this.setState(prevState => ({total: prevState.total - prevState.currentValue}));
+    }
+    else if (this.state.currentOperator === '*') {
+      this.setState(prevState => ({total: prevState.total * prevState.currentValue}));
+    }
+    else if (this.state.currentOperator === '/') {
+      this.setState(prevState => ({total: prevState.total / prevState.currentValue}));
+    }
+    else {
+      this.setState(prevState => ({total: prevState.currentValue}));
     }
   }
 
   render() {
     return (
-      <div className="App">
-        <div id="outputDisplay">{this.state.total}</div>
-        <button onClick={() => this.buttonClicked(0)}>0</button>
-        <button onClick={() => this.buttonClicked(1)}>1</button>
-        <button onClick={() => this.buttonClicked(2)}>2</button>
-        <button onClick={() => this.buttonClicked(3)}>3</button>
-        <button onClick={() => this.buttonClicked(4)}>4</button>
-        <button onClick={() => this.buttonClicked(5)}>5</button>
-        <button onClick={() => this.buttonClicked(6)}>6</button>
-        <button onClick={() => this.buttonClicked(7)}>7</button>
-        <button onClick={() => this.buttonClicked(8)}>8</button>
-        <button onClick={() => this.buttonClicked(9)}>9</button>
-        <button onClick={() => this.buttonClicked('+')}>+</button>    
+      <div className="calculator">
+        <div id="output-display">{ this.state.showTotal ? this.state.total : this.state.currentValue }</div>
+        <button className="calculator-button number-input" id="button-0" onClick={() => this.buttonClicked(0)}>0</button>
+        <button className="calculator-button number-input" id="button-1" onClick={() => this.buttonClicked(1)}>1</button>
+        <button className="calculator-button number-input" id="button-2" onClick={() => this.buttonClicked(2)}>2</button>
+        <button className="calculator-button number-input" id="button-3" onClick={() => this.buttonClicked(3)}>3</button>
+        <button className="calculator-button number-input" id="button-4" onClick={() => this.buttonClicked(4)}>4</button>
+        <button className="calculator-button number-input" id="button-5" onClick={() => this.buttonClicked(5)}>5</button>
+        <button className="calculator-button number-input" id="button-6" onClick={() => this.buttonClicked(6)}>6</button>
+        <button className="calculator-button number-input" id="button-7" onClick={() => this.buttonClicked(7)}>7</button>
+        <button className="calculator-button number-input" id="button-8" onClick={() => this.buttonClicked(8)}>8</button>
+        <button className="calculator-button number-input" id="button-9" onClick={() => this.buttonClicked(9)}>9</button>
+        <button className="calculator-button operator-input" id="button-add" onClick={() => this.buttonClicked('+')}>+</button>
+        <button className="calculator-button operator-input" id="button-subtract" onClick={() => this.buttonClicked('-')}>-</button>
+        <button className="calculator-button operator-input" id="button-multiply" onClick={() => this.buttonClicked('*')}>*</button>
+        <button className="calculator-button operator-input" id="button-divide" onClick={() => this.buttonClicked('/')}>/</button>
+        <button className="calculator-button operator-input" id="button-equals" onClick={() => this.buttonClicked('=')}>=</button>        
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+  
+  it('renders 10 number buttons', () => {
+    const app = shallow(<App />);
+    expect(app.find('.number-input')).to.have.length(10);
+  });
+  
+  it('renders 5 operator buttons', () => {
+    const app = shallow(<App />);
+    expect(app.find('.operator-input')).to.have.length(5);
+  });
+
+  // Separated out since I plan on making display its own component
+  const isDisplayShowing = (app, value) => {
+    expect(app.find('#output-display').text()).to.equal(value.toString());
+  }
+
+  /* Number button tests */
+  const numberInputTest = num => {
+    it(`renders only one ${num} button`), () => {
+      const app = shallow(<App />);
+      expect(app.find(`#button-${num}`)).to.have.length(1);
+    };
+
+    it(`renders ${num} when ${num} button is pressed`, () => {
+      const app = shallow(<App />);
+      app.find(`#button-${num}`).simulate('click');
+      isDisplayShowing(app, num);
+    });
+  };
+
+  numberInputTest(0);
+  numberInputTest(1);
+  numberInputTest(2);
+  numberInputTest(3);
+  numberInputTest(4);
+  numberInputTest(5);
+  numberInputTest(6);
+  numberInputTest(7);
+  numberInputTest(8);
+  numberInputTest(9);
+  
 });
+
